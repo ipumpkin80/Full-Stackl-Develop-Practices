40673b63634f0ef65aef1984c66a964aad80fff4
Paul Nguyen <86182541+ipumpkin80@users.noreply.github.com>
2025-03-17 19:03:53 +0200

    Create vote_checker.py

---
+++ vote_checker.py
@@ -0,0 +1,8 @@
+# Nhập độ tuổi của người dùng
+age = int(input("Nhập độ tuổi của bạn: "))
+
+# Kiểm tra điều kiện đủ điều kiện bỏ phiếu
+if age >= 18:
+    print("Bạn đủ điều kiện bỏ phiếu")
+else:
+    print("Bạn chưa đủ điều kiện bỏ phiếu")
